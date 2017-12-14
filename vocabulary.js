9a0aa78f9dba0b4d0157b7d33bb36b2eb1f8166e
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2017-12-14 00:17:15 +0000

    Extend word list.

--- vocabulary.js
+++ vocabulary.js
@@ -1,18 +1,41 @@
 var both = new Map();
+
+both.set('dh', '<span class="digraph">dh</span>');
+both.set('H', '<span class="thick">h</span>');
+both.set('ch', '<span class="digraph">ch</span>');
+both.set('kh', '<span class="digraph">kh</span>');
+both.set('T', '<span class="thick">t</span>');
+both.set('KH', '<span class="digraph">k<span class="hebrew">h</span></span>');
+both.set('AA', '<span class="ayin digraph">aa</span>');
+both.set('gh', '<span class="digraph">gh</span>');
 both.set('S', '<span class="thick">s</span>');
 both.set('D', '<span class="thick">d</span>');
-both.set('KH', 'k<span class="hebrew">h</span>');
-both.set('AA', '<span class="ayin digraph">aa</span>');
-both.set('SH', '<span class="digraph">sh</span>');
-both.set('v', 'b');
+both.set('DH', '<span class="digraph thick">dh</span>');
+both.set('sh', '<span class="digraph">sh</span>');
+both.set('th', '<span class="digraph">th</span>');
+both.set('E', '<span class="thin-e">e</span>');
 both.set('eh', '<span class="tsere-trans">eh</span>');
 both.set('OO', '<span class="long">-o-</span>');
+
+both.set('v', 'b');
+both.set('g-', both.get('gh'));
+both.set('d-', both.get('dh'));
+both.set('t-', both.get('th'));
+both.set('j', 'g');
+both.set('ts', both.get('S'));
 both.set('-', '.');
 
 function transcribeBoth(transcription) {
 	var output = '';
-	for (let items of transcription) {
-		output = output + `<td colspan="${items.length}"class="english-char">`;
+	for (let i = 0; i < transcription.length; i++) {
+		let items = transcription[i];
+		let endOfSyllable = (i == transcription.length - 1 || transcription[i + 1][0] == '-');
+		output = output + `<td colspan="${items.length}" class="english-char`;
+		if (endOfSyllable && i > 0) {
+			output = output + ' align-left';
+		}
+		output = output + '">';
+
 		for (let phone of items) {
 			var html = both.get(phone);
 			if (html === undefined) {
